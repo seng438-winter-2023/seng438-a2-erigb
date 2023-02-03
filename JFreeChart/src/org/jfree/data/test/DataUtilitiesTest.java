63daaf78b56d6099f2203d0e8b6b7f9d85a55384
danijourdain <danijourdain@icloud.com>
2023-02-03 10:32:06 -0700

    got Range test working, startedwork on DataUtilites test

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -2,21 +2,29 @@ package org.jfree.data.test;
 
 import static org.junit.Assert.*;
 
+import java.security.InvalidParameterException;
+
 import org.jfree.data.DataUtilities;
 import org.jfree.data.Values2D;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class DataUtilitiesTest {
-
-	 @Test
-	 public void calculateColumnTotalForTwoValues() {
-	     // setup
-	     Mockery mockingContext = new Mockery();
-	     final Values2D values = mockingContext.mock(Values2D.class);
-	     mockingContext.checking(new Expectations() {
-	         {
+	Values2D values;
+	
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+	
+    @Before
+    public void setUp() throws Exception { 
+    	 Mockery mockObject = new Mockery();
+    	 values = mockObject.mock(Values2D.class);
+    	 mockObject.checking(new Expectations() {
+    		 {
 	             one(values).getRowCount();
 	             will(returnValue(2));
 	             one(values).getValue(0, 0);
@@ -24,12 +32,20 @@ public class DataUtilitiesTest {
 	             one(values).getValue(1, 0);
 	             will(returnValue(2.5));
 	         }
-	     });
+    	 });
+    }
+
+	 @Test
+	 public void calculateColumnTotalForTwoValues() {
 	     double result = DataUtilities.calculateColumnTotal(values, 0);
-	     // verify
 	     assertEquals(result, 10.0, .000000001d);
-	     // tear-down: NONE in this test method
+
 	 }
 
+//	 @Test
+//	 public void invalidParameterExceptionIsThrown() {
+//		 exception.expect(InvalidParameterException.class);
+//		 DataUtilities.calculateColumnTotal(values, 10);
+//	 }
 
 }
--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*; import org.jfree.data.Range; import org.junit.
 
 public class RangeTest {
     private Range exampleRange;
+    private Range nullRange;
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
@@ -11,13 +12,26 @@ public class RangeTest {
     @Before
     public void setUp() throws Exception { 
     	exampleRange = new Range(-1, 1);
+    	nullRange = null;
     }
 
 
     @Test
     public void lengthShouldBeTwo() {
         assertEquals("The length of Range from -1 to 1 should be 2",
-        2, exampleRange.getLength(), .000000001d);
+        		2, exampleRange.getLength(), .000000001d);
+    }
+    
+    @Test
+    public void containsShouldBeTrue() {
+    	assertTrue("The Range does contain value 0.5. Contains should return true",
+    	exampleRange.contains(0.5));
+    }
+    
+    @Test
+    public void containsShouldBeFalse() {
+    	assertFalse("The Range does contain value 0.5. Contains should return true",
+    	exampleRange.contains(-7));
     }
 
     @After
