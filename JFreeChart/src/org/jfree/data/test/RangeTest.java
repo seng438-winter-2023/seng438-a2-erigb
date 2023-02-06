4984633924e6551efbbfdde9c150925ed1028298
Mohammed Alshoura <mohammed.alshoura@ucalgary.ca>
2023-02-06 00:51:53 -0700

    Update RangeTest.java

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -32,6 +32,38 @@ public class RangeTest {
     	assertFalse("The Range does contain value 0.5. Contains should return true",
     	exampleRange.contains(-7));
     }
+    
+    @Test
+    public void shiftRangeWithoutZeroCrossingPositveDelta() {
+    	Range afterShift = Range.shift(exampleRange, 0.5);
+    	
+    	assertEquals("The lower bound should be 0.5 + -1 = -0.5",
+    	        -0.5, afterShift.getLowerBound(), .000000001d);
+    	assertEquals("The upper bound should be 0.5 + 1 = 1.5",
+    	        1.5, afterShift.getUpperBound(), .000000001d);
+    	
+    }
+    
+    @Test
+    public void shiftRangeWithoutZeroCrossingNegativeDelta() {
+    	Range afterShift = Range.shift(exampleRange, -0.5);
+    	
+    	assertEquals("The lower bound should be -0.5 + -1 = -1.5",
+    	        -1.5, afterShift.getLowerBound(), .000000001d);
+    	assertEquals("The upper bound should be -0.5 + 1 = 0.5",
+    	        0.5, afterShift.getUpperBound(), .000000001d);
+    	
+    }
+
+    @Test
+    public void shiftShouldNotAllowZeroCrossing() {
+    	Range afterShift = Range.shift(exampleRange, 5.0);
+    	assertEquals("The upper bound should be 5.0 + 1 = 6.0",
+    	        6.0, afterShift.getUpperBound(), .000000001d);
+    	assertEquals("The lower bound should be 0 since no zero crossing is allowed and -1 + 5 = 4 > 0",
+    	        0.0, afterShift.getLowerBound(), .000000001d);
+    	
+    }
 
     @After
     public void tearDown() throws Exception {
