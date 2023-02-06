68b853ee983836b73607bcc1499c6a42dd6e1c6c
erigb <ethanrigby@live.com>
2023-02-06 09:46:57 -0700

    Added in tests
    Dani, I had to change where you initialize your mockobject because it was breaking any that I tried to make if it was created in the before.

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -15,13 +15,18 @@ import org.junit.rules.ExpectedException;
 
 public class DataUtilitiesTest {
 	Values2D values;
+	Values2D rowValues;
 	
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
 	
     @Before
-    public void setUp() throws Exception { 
-    	 Mockery mockObject = new Mockery();
+    public void setUp() throws Exception {
+    }
+
+	 @Test
+	 public void calculateColumnTotalForTwoValues() {
+		 Mockery mockObject = new Mockery();
     	 values = mockObject.mock(Values2D.class);
     	 mockObject.checking(new Expectations() {
     		 {
@@ -33,19 +38,62 @@ public class DataUtilitiesTest {
 	             will(returnValue(2.5));
 	         }
     	 });
-    }
-
-	 @Test
-	 public void calculateColumnTotalForTwoValues() {
 	     double result = DataUtilities.calculateColumnTotal(values, 0);
-	     assertEquals(result, 10.0, .000000001d);
+	     assertEquals("The total for the column containing 2.5 and 7.5 is 10.0",10.0, result, .000000001d);
 
 	 }
-
-//	 @Test
-//	 public void invalidParameterExceptionIsThrown() {
-//		 exception.expect(InvalidParameterException.class);
-//		 DataUtilities.calculateColumnTotal(values, 10);
-//	 }
-
+	 
+	 @Test
+	 public void calculateColumnTotalReturnsZeroForInvalidValue() {
+		 Mockery mockObject = new Mockery();
+    	 values = mockObject.mock(Values2D.class);
+    	 mockObject.checking(new Expectations() {
+    		 {
+	             one(values).getRowCount();
+	             will(returnValue(2));
+	             one(values).getValue(0, 0);
+	             will(returnValue(0));
+	             one(values).getValue(1, 0);
+	             will(returnValue(0));
+	         }
+    	 });
+		 double result = DataUtilities.calculateColumnTotal(values, 0);
+		 assertEquals(0, result, .000000001d);
+	 }
+	 
+	 @Test
+	 public void calculateRowTotalForTwoValues() {
+		 Mockery mockObject = new Mockery();
+    	 values = mockObject.mock(Values2D.class);
+    	 mockObject.checking(new Expectations() {
+    		 {
+	             one(values).getColumnCount();
+	             will(returnValue(2));
+	             one(values).getValue(0, 0);
+	             will(returnValue(7.5));
+	             one(values).getValue(0, 1);
+	             will(returnValue(2.5));
+	         }
+    	 });
+	     double result = DataUtilities.calculateRowTotal(values, 0);
+	     assertEquals("The total for the row containing 2.5 and 7.5 is 10.0",7.5, result, .000000001d);
+	 }
+	 
+	 @Test
+	 public void calculateRowTotalReturnsZeroForInvalidValue() {
+		 Mockery mockObject = new Mockery();
+    	 values = mockObject.mock(Values2D.class);
+    	 mockObject.checking(new Expectations() {
+    		 {
+	             one(values).getColumnCount();
+	             will(returnValue(2));
+	             one(values).getValue(0, 0);
+	             will(returnValue(0));
+	             one(values).getValue(0, 10);
+	             will(returnValue(0));
+	         }
+    	 });
+	     double result = DataUtilities.calculateRowTotal(values, 0);
+	     assertEquals("The total for the row should be 0 with invalid values", 0, result, .000000001d);
+	 }
 }
--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -33,6 +33,19 @@ public class RangeTest {
     	assertFalse("The Range does contain value 0.5. Contains should return true",
     	exampleRange.contains(-7));
     }
+    
+    
+    @Test
+    public void intersectsRangeOverlapsSpecifiedRange() {
+    	assertTrue("The Range intersects the current range (-1, 1). Contains should return true",
+    	exampleRange.intersects(-1, 1));
+    }
+    
+    @Test
+    public void intersectsRangeDoesNotOverlapsSpecifiedRange() {
+    	assertFalse("The Range does not intersects the current range (-1, 1). Contains should return false",
+    	exampleRange.intersects(2, 3));
+    }
 
     @After
     public void tearDown() throws Exception {
